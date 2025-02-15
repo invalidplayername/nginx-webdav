1f3ac5446f40f4ff4ecc3e8b0cb85e8f82cc9d7f
weruda <weruda@gmx.de>
2025-02-15 18:14:23 +0100

    large file support

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM nginx:latest
 
-RUN apt-get update && apt-get install -y apache2-utils && rm -rf /var/lib/apt/lists/*
+RUN apt-get update && apt-get install -y apache2-utils gettext && rm -rf /var/lib/apt/lists/*
 
-COPY nginx.conf /etc/nginx/nginx.conf
-
-RUN mkdir -p /var/www/webdav
+COPY nginx.conf.template /etc/nginx/nginx.conf.template
 
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
+
+RUN mkdir -p /var/www/webdav
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,16 @@ fi
 
 WEBDAV_UID=${WEBDAV_UID:-1000}
 WEBDAV_GID=${WEBDAV_GID:-1000}
+WEBDAV_MAX_BODY_SIZE=${WEBDAV_MAX_BODY_SIZE:-1G}
+WEBDAV_TIMEOUT=${WEBDAV_TIMEOUT:-600s}
 
 addgroup -g "$WEBDAV_GID" webdavgroup
 adduser -D -u "$WEBDAV_UID" -G webdavgroup webdavuser
 
-#chown -R "$WEBDAV_UID":"$WEBDAV_GID" /var/www/webdav
+chown -R "$WEBDAV_UID":"$WEBDAV_GID" /var/www/webdav
 
 echo "$WEBDAV_PASSWORD" | htpasswd -c -i /etc/nginx/htpasswd "$WEBDAV_USERNAME"
+
+envsubst '$WEBDAV_MAX_BODY_SIZE $WEBDAV_TIMEOUT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec nginx -g "daemon off;"
